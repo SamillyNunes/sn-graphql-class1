cfc4c5e72917ba43a8fcd0342c5fb7ec8d2a56d3
Samilly <samillynunes19@gmail.com>
2022-06-29 21:07:40 -0300

    App and video module developed, dependencies modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { VideosModule } from './videos/videos.module';
+// import {ApolloDriv}
+
 
 @Module({
-  imports: [VideosModule],
+  imports: [
+    VideosModule, 
+    GraphQLModule.forRoot(
+      {
+        typePaths: ['./**/*.graphql']
+      }
+    ),
+  ],
   controllers: [],
   providers: [],
 })
--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { VideoResolver } from './videos.resolver';
+import { VideoService } from './videos.service';
 
-@Module({})
+//providers: injecao de dependencia
+@Module({
+    providers: [VideoService, VideoResolver]
+})
 export class VideosModule {}
